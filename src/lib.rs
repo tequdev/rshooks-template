822f2c97b85926c8179c5256499cdf02fd863de5
tequ <git@tequ.dev>
2024-09-21 12:36:17 +0900

    autotransfer

--- src/lib.rs
+++ src/lib.rs
@@ -1,32 +1,133 @@
-//! A hook template
-
 #![no_std]
+use rshooks_api::{
+    _c::{ttURITOKEN_CREATE_SELL_OFFER, INVALID_TXN},
+    *,
+};
 
-use rshooks_api::*;
+#[rustfmt::skip]
+static mut TXN: [u8;250] =  [
+    /* size,upto */
+    /*   3,  0  */ 0x12, 0x00, 0x2F,                                                 /* tt = URITokenBuy */
+    /*   5,  3  */ 0x22, 0x80, 0x00, 0x00, 0x00,                                     /* flags = tfCanonical */
+    /*   5,  8  */ 0x24, 0x00, 0x00, 0x00, 0x00,                                     /* sequence = 0 */
+    /*   6, 13  */ 0x20, 0x1A, 0x00, 0x00, 0x00, 0x00,                               /* first ledger seq */
+    /*   6, 19  */ 0x20, 0x1B, 0x00, 0x00, 0x00, 0x00,                               /* last ledger seq */
+    /*  8, 25  */ 0x61, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,              /* amount field 9 or 49 bytes */
+    
+    /*  34, 33  */ 0x50, 0x24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    /* hash256 = URITokenID  */
+    /*   9, 67 */ 0x68, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,                   /* fee      */
+    0x00,
+    /*  35, 76 */ 0x73, 0x21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* pubkey   */
+    /*  22, 111 */ 0x81, 0x14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                                        /* src acc  */
+    /* 116, 133 */ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* emit details */
+    /*   0, 249 */
+];
 
-const GUARD_ID_1: u32 = line!();
+// TX BUILDER
 
 #[no_mangle]
-pub extern "C" fn cbak(_: u32) -> i64 { 
-    return 0;
-}
+pub extern "C" fn hook(_reserved: u32) -> i64 {
+    _g(1, 1);
 
-#[no_mangle]
-pub extern "C" fn hook(_: i64) -> i64 {
-    // Every hook needs to import guard function 
-    // and use it at least once
-    _g(GUARD_ID_1, 1);
-    
-    let mut account: AccountId = uninit_buf!();
-    let _ = hook_account(&mut account);
-    
-    let mut raddr: Buffer<40> = uninit_buf!();
-    let _ = util_raddr(&mut raddr, &account);
+    let FLS_OUT = unsafe { &mut TXN[15..19] };
+    let LLS_OUT = unsafe { &mut TXN[21..25] };
+    let INDEXID_OUT = unsafe { &mut TXN[36..68] };
+    let AMOUNT_OUT = unsafe { &mut TXN[25..34] };
+    let HOOK_ACC = unsafe { &mut TXN[114..134] };
+    let FEE_OUT = unsafe { &mut TXN[69..77] };
+    let EMIT_OUT = unsafe { &mut TXN[134..250] };
 
-    // Tracing when compiling in debug mode
-    #[cfg(debug_assertions)]
-    let _  = trace(b"Accept: called", &raddr, DataRepr::AsUTF8);
+    let _ = trace(b"autotransfer.c: Called.", b"", DataRepr::AsUTF8);
 
-    // Accept all
-    accept(b"Accept: accepted", 0)
+    // HOOK ON: TT
+    let tt = otxn_type();
+    if tt != ttURITOKEN_CREATE_SELL_OFFER.into() {
+        rollback(
+            b"autotransfer.c: HookOn field is incorrectly set.",
+            INVALID_TXN.into(),
+        );
+    }
+
+    // ACCOUNT: Hook Account
+    let _ = hook_account(&mut HOOK_ACC[0..]);
+
+    // ACCOUNT: Origin Tx Account
+    let mut otx_acc = [0u8; 20];
+    let _ = otxn_field(&mut otx_acc, FieldId::Account);
+
+    // FILTER ON: ACCOUNT
+    if is_buffer_equal::<20>(&HOOK_ACC, &otx_acc) {
+        accept(b"autotransfer.c: outgoing tx on `Account`.", 0);
+    }
+
+    let mut tid_buffer = [0u8; 32];
+    match otxn_field(&mut tid_buffer, FieldId::URITokenID) {
+        Ok(32) => (),
+        _ => {
+            rollback(b"autotransfer.c: Invalid URITokenID", INVALID_TXN.into());
+        }
+    }
+    INDEXID_OUT.copy_from_slice(&tid_buffer);
+
+    // TXN: PREPARE: Init
+    let _ = etxn_reserve(1);
+
+    // TXN PREPARE: FirstLedgerSequence
+    let fls = (ledger_seq() as u32) + 1;
+    FLS_OUT.copy_from_slice(&fls.to_be_bytes());
+
+    // TXN PREPARE: LastLedgerSequense
+    let lls = fls + 4;
+    LLS_OUT.copy_from_slice(&lls.to_be_bytes());
+
+    // // TXN PREPARE: Amount
+    let drops: u64 = 0;
+    {
+        let b = &mut AMOUNT_OUT[1..];
+        b[0] = 0b01000000 + ((drops >> 56) & 0b00111111) as u8;
+        b[1] = ((drops >> 48) & 0xFF) as u8;
+        b[2] = ((drops >> 40) & 0xFF) as u8;
+        b[3] = ((drops >> 32) & 0xFF) as u8;
+        b[4] = ((drops >> 24) & 0xFF) as u8;
+        b[5] = ((drops >> 16) & 0xFF) as u8;
+        b[6] = ((drops >> 8) & 0xFF) as u8;
+        b[7] = (drops & 0xFF) as u8;
+    }
+
+    // TXN PREPARE: Emit Metadata
+    let _ = etxn_details(&mut EMIT_OUT[0..]);
+
+    // // TXN PREPARE: Fee
+    let fee = match etxn_fee_base(unsafe { &TXN[0..] }) {
+        Ok(fee) => fee,
+        Err(e) => {
+            rollback(b"autotransfer.c: Failed to get fee.", e.code() as i64);
+        }
+    };
+
+    {
+        let b = &mut FEE_OUT[0..];
+        b[0] = 0b01000000 + ((fee >> 56) & 0b00111111) as u8;
+        b[1] = ((fee >> 48) & 0xFF) as u8;
+        b[2] = ((fee >> 40) & 0xFF) as u8;
+        b[3] = ((fee >> 32) & 0xFF) as u8;
+        b[4] = ((fee >> 24) & 0xFF) as u8;
+        b[5] = ((fee >> 16) & 0xFF) as u8;
+        b[6] = ((fee >> 8) & 0xFF) as u8;
+        b[7] = (fee & 0xFF) as u8;
+    }
+
+    // TXN: Emit/Send Txn
+    let mut emithash = [0u8; 32];
+    let emit_result = match emit(&mut emithash, unsafe { &mut TXN[0..] }) {
+        Ok(result) => result,
+        Err(_) => {
+            rollback(b"autotransfer: Failed to emit tx.", INVALID_TXN.into());
+        }
+    };
+
+    if emit_result > 0 {
+        accept(b"autotransfer: Tx emitted success.", 0);
+    }
+    accept(b"autotransfer: Tx emitted failure.", 0);
 }
--- test/integration/index.test.ts
+++ test/integration/index.test.ts
@@ -1,69 +1,120 @@
-import { SetHookFlags } from '@transia/xrpl'
-
+// src
 import {
+  ExecutionUtility,
   type SetHookParams,
-  type XrplIntegrationTestContext,
   Xrpld,
   clearAllHooksV3,
   clearHookStateV3,
   createHookPayload,
   hexNamespace,
   type iHook,
-  serverUrl as localServerUrl,
   setHooksV3,
+} from '@transia/hooks-toolkit'
+// xrpl-helpers
+import {
+  type XrplIntegrationTestContext,
+  close,
+  serverUrl,
   setupClient,
   teardownClient,
-} from '@transia/hooks-toolkit'
+} from '@transia/hooks-toolkit/dist/npm/src/libs/xrpl-helpers'
+import {
+  type AccountSet,
+  AccountSetAsfFlags,
+  SetHookFlags,
+  type TransactionMetadata,
+  type URITokenCreateSellOffer,
+  type URITokenMint,
+  convertStringToHex,
+  xrpToDrops,
+} from '@transia/xrpl'
+import { hashURIToken } from '@transia/xrpl/dist/npm/utils/hashes'
 
-const namespace = 'namespace'
+// AutoTransfer: ACCEPT: success
 
-const serverUrl = localServerUrl
-
-describe('test', () => {
+describe('autotransfer', () => {
   let testContext: XrplIntegrationTestContext
 
   beforeAll(async () => {
     testContext = await setupClient(serverUrl)
+    const hookWallet = testContext.hook1
     const hook = createHookPayload({
       version: 0,
       createFile: 'index',
-      namespace: namespace,
-      flags: SetHookFlags.hsfOverride,
-      hookOnArray: ['Invoke'],
+      namespace: 'autotransfer',
+      flags: SetHookFlags.hsfCollect + SetHookFlags.hsfOverride,
+      hookOnArray: ['URITokenCreateSellOffer'],
     })
     await setHooksV3({
       client: testContext.client,
-      seed: testContext.alice.seed,
+      seed: hookWallet.seed,
       hooks: [{ Hook: hook }],
     } as SetHookParams)
   })
-
   afterAll(async () => {
-    const clearHook: iHook = {
-      Flags: SetHookFlags.hsfNSDelete,
-      HookNamespace: hexNamespace(namespace),
-    }
-    await clearHookStateV3({
-      client: testContext.client,
-      seed: testContext.alice.seed,
-      hooks: [{ Hook: clearHook }],
-    } as SetHookParams)
     await clearAllHooksV3({
       client: testContext.client,
-      seed: testContext.alice.seed,
+      seed: testContext.hook1.seed,
     } as SetHookParams)
-    await teardownClient(testContext)
+
+    const clearHook = {
+      Flags: SetHookFlags.hsfNSDelete,
+      HookNamespace: hexNamespace('autotransfer'),
+    } as iHook
+    await clearHookStateV3({
+      client: testContext.client,
+      seed: testContext.hook1.seed,
+      hooks: [{ Hook: clearHook }],
+    } as SetHookParams)
+    teardownClient(testContext)
   })
 
-  it('', async () => {
-    const response = await Xrpld.submit(testContext.client, {
-      tx: {
-        TransactionType: 'Invoke',
-        Account: testContext.alice.address,
-      },
-      wallet: testContext.alice,
+  it('autotransfer - success', async () => {
+    const hookWallet = testContext.hook1
+    const aliceWallet = testContext.alice
+
+    // AccountSet: Hook Acct
+    const asTx: AccountSet = {
+      TransactionType: 'AccountSet',
+      Account: hookWallet.classicAddress,
+      SetFlag: AccountSetAsfFlags.asfTshCollect,
+    }
+    await Xrpld.submit(testContext.client, {
+      wallet: hookWallet,
+      tx: asTx,
     })
-    console.log(response.meta)
-    expect(response.meta).toHaveProperty('HookExecutions')
+    // biome-ignore lint/style/useTemplate: <explanation>
+    const uritokenString = 'ipfs://autotransfer' + Math.random()
+    // URITokenMint
+    const mintTx: URITokenMint = {
+      TransactionType: 'URITokenMint',
+      Account: aliceWallet.classicAddress,
+      URI: convertStringToHex(uritokenString),
+    }
+    await Xrpld.submit(testContext.client, {
+      wallet: aliceWallet,
+      tx: mintTx,
+    })
+    const uriTokenID = hashURIToken(aliceWallet.classicAddress, uritokenString)
+
+    // URITokenCreateSellOffer
+    const builtTx1: URITokenCreateSellOffer = {
+      TransactionType: 'URITokenCreateSellOffer',
+      Account: aliceWallet.classicAddress,
+      Amount: xrpToDrops(0),
+      Destination: hookWallet.classicAddress,
+      URITokenID: uriTokenID,
+    }
+    const result1 = await Xrpld.submit(testContext.client, {
+      wallet: aliceWallet,
+      tx: builtTx1,
+    })
+    await close(testContext.client)
+
+    const hookExecutions1 = await ExecutionUtility.getHookExecutionsFromMeta(
+      testContext.client,
+      result1.meta as TransactionMetadata,
+    )
+    expect(hookExecutions1.executions[0].HookReturnString).toMatch('autotransfer: Tx emitted success')
   })
 })
