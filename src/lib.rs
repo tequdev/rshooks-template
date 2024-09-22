9667aee87280fdc46009a5716b5343823df88074
tequ <git@tequ.dev>
2024-09-22 18:06:54 +0900

    refactor

--- src/lib.rs
+++ src/lib.rs
@@ -9,59 +9,75 @@ static mut TXN: [u8;250] =  [
     /*   5,  8  */ 0x24, 0x00, 0x00, 0x00, 0x00,                                     /* sequence = 0 */
     /*   6, 13  */ 0x20, 0x1A, 0x00, 0x00, 0x00, 0x00,                               /* first ledger seq */
     /*   6, 19  */ 0x20, 0x1B, 0x00, 0x00, 0x00, 0x00,                               /* last ledger seq */
-    /*  8, 25  */ 0x61, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,              /* amount field 9 or 49 bytes */
-    
-    /*  34, 33  */ 0x50, 0x24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,    /* hash256 = URITokenID  */
-    /*   9, 67 */ 0x68, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,                   /* fee      */
-    0x00,
-    /*  35, 76 */ 0x73, 0x21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* pubkey   */
-    /*  22, 111 */ 0x81, 0x14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                                        /* src acc  */
-    /* 116, 133 */ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, /* emit details */
+    /*   8, 25  */ 0x61, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99, 0x99,             /* amount field 9 or 49 bytes */
+    /*  34, 33  */ 0x50, 0x24, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,     /* hash256 = URITokenID  */
+    /*   9, 67  */ 0x68, 0x40, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00,             /* fee      */
+    /*  35, 76  */ 0x73, 0x21, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,  /* pubkey   */
+    /*  22, 111 */ 0x81, 0x14, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,                                         /* src acc  */
+    /* 116, 133 */ 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                   0, 0, 0, 0, 0,                                                    /* emit details */
     /*   0, 249 */
 ];
 
-// TX BUILDER
+#[inline]
+fn get_txn_field_ptr<const T: usize>(offset: usize) -> &'static mut Buffer<T> {
+    unsafe { &mut *(TXN[offset..].as_mut_ptr() as *mut Buffer<T>) }
+}
 
 #[no_mangle]
 pub extern "C" fn hook(_reserved: u32) -> i64 {
     _g(1, 1);
 
     let txn = unsafe { &mut TXN[0..] };
-    let fls_out = unsafe { &mut TXN[15..19] };
-    let lls_out = unsafe { &mut TXN[21..25] };
-    let indexid_out = unsafe { &mut TXN[36..68] };
-    let amount_out = unsafe { &mut TXN[25..34] };
-    let hook_acc = unsafe { &mut TXN[114..134] };
-    let fee_out = unsafe { &mut TXN[69..77] };
-    let emit_out = unsafe { &mut TXN[134..250] };
+    let indexid_out = get_txn_field_ptr::<32>(36);
+    let amount_out = get_txn_field_ptr::<8>(26);
+    let hook_acc = get_txn_field_ptr::<20>(114);
 
-    trace(b"autotransfer.c: Called.", b"", DataRepr::AsUTF8).unwrap();
+    trace(b"autotransfer: Called.", b"", DataRepr::AsUTF8).unwrap();
 
     // HOOK ON: TT
-    let tt = otxn_type();
+    let tt: i64 = otxn_type();
     if tt != TxnType::URITokenCreateSellOffer as i64 {
         rollback(
-            b"autotransfer.c: HookOn field is incorrectly set.",
+            b"autotransfer: HookOn field is incorrectly set.",
             INVALID_TXN.into(),
         );
     }
 
-    // ACCOUNT: Hook Account
-    hook_account(&mut hook_acc[0..]).unwrap();
-
-    // ACCOUNT: Origin Tx Account
-    let mut otx_acc: AccountId = [0u8; 20];
-    otxn_field(&mut otx_acc, FieldId::Account).unwrap();
+    let otx_acc: &mut AccountId = &mut [0u8; 20];
 
     // FILTER ON: ACCOUNT
-    if is_buffer_equal::<20>(&hook_acc, &otx_acc) {
-        accept(b"autotransfer.c: outgoing tx on `Account`.", 0);
+    if is_txn_outgoing::<20>(hook_acc, otx_acc).unwrap() {
+        accept(b"autotransfer: outgoing tx on `Account`.", 0);
     }
 
-    otxn_field(&mut indexid_out[0..], FieldId::URITokenID).unwrap();
+    // TXN: PREPARE: URITokenID
+    otxn_field(indexid_out, FieldId::URITokenID).unwrap();
 
     // TXN: PREPARE: Init
-    let _ = etxn_reserve(1);
+    etxn_reserve(1).unwrap();
+
+    // TXN PREPARE: Buy Amount
+    let drops: u64 = 0;
+    encode_amount(amount_out, drops);
+
+    autofill_txn(txn);
+
+    // TXN: Emit/Send Txn
+    let emithash = &mut [0u8; 32];
+    emit(emithash, txn).expect(b"autotransfer: Tx emitted failure.");
+
+    accept(b"autotransfer: Tx emitted success.", 0);
+}
+
+#[inline]
+fn autofill_txn(txn: &mut [u8]) {
+    let fls_out = get_txn_field_ptr::<4>(15);
+    let lls_out = get_txn_field_ptr::<4>(21);
+    let fee_out = get_txn_field_ptr::<8>(69);
+    let emit_out = get_txn_field_ptr::<116>(134);
 
     // TXN PREPARE: FirstLedgerSequence
     let fls = (ledger_seq() as u32) + 1;
@@ -71,40 +87,22 @@ pub extern "C" fn hook(_reserved: u32) -> i64 {
     let lls = fls + 4;
     lls_out.copy_from_slice(&lls.to_be_bytes());
 
-    // // // TXN PREPARE: Amount
-    let drops: u64 = 0;
-    {
-        let b = &mut amount_out[1..];
-        b[0] = 0b01000000 + ((drops >> 56) & 0b00111111) as u8;
-        b[1] = ((drops >> 48) & 0xFF) as u8;
-        b[2] = ((drops >> 40) & 0xFF) as u8;
-        b[3] = ((drops >> 32) & 0xFF) as u8;
-        b[4] = ((drops >> 24) & 0xFF) as u8;
-        b[5] = ((drops >> 16) & 0xFF) as u8;
-        b[6] = ((drops >> 8) & 0xFF) as u8;
-        b[7] = ((drops >> 0) & 0xFF) as u8;
-    }
-
     // TXN PREPARE: Emit Metadata
-    let _ = etxn_details(&mut emit_out[0..]);
+    etxn_details(emit_out).unwrap();
 
-    // // TXN PREPARE: Fee
-    let fee = etxn_fee_base(unsafe { &TXN[0..] }).expect(b"autotransfer.c: Failed to get fee.");
+    // TXN PREPARE: Fee
+    let fee = etxn_fee_base(txn).expect(b"autotransfer: Failed to get fee.");
+    encode_amount(fee_out, fee as u64);
+}
 
-    {
-        let b = &mut fee_out[0..];
-        b[0] = 0b01000000 + ((fee >> 56) & 0b00111111) as u8;
-        b[1] = ((fee >> 48) & 0xFF) as u8;
-        b[2] = ((fee >> 40) & 0xFF) as u8;
-        b[3] = ((fee >> 32) & 0xFF) as u8;
-        b[4] = ((fee >> 24) & 0xFF) as u8;
-        b[5] = ((fee >> 16) & 0xFF) as u8;
-        b[6] = ((fee >> 8) & 0xFF) as u8;
-        b[7] = ((fee >> 0) & 0xFF) as u8;
-    }
-
-    // TXN: Emit/Send Txn
-    let mut emithash = [0u8; 32];
-    emit(&mut emithash, txn).expect(b"autotransfer: Tx emitted failure.");
-    accept(b"autotransfer: Tx emitted success.", 0);
+#[inline]
+fn encode_amount(out: &mut NativeAmount, amount: u64) {
+    out[0] = 0b01000000 + ((amount >> 56) & 0b00111111) as u8;
+    out[1] = ((amount >> 48) & 0xFF) as u8;
+    out[2] = ((amount >> 40) & 0xFF) as u8;
+    out[3] = ((amount >> 32) & 0xFF) as u8;
+    out[4] = ((amount >> 24) & 0xFF) as u8;
+    out[5] = ((amount >> 16) & 0xFF) as u8;
+    out[6] = ((amount >> 8) & 0xFF) as u8;
+    out[7] = ((amount >> 0) & 0xFF) as u8;
 }
