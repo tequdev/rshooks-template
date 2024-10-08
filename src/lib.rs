a69c23259efe04dff63f9eea10a050f3b44eab38
tequ <git@tequ.dev>
2024-10-08 16:26:39 +0900

    format

--- src/lib.rs
+++ src/lib.rs
@@ -7,25 +7,25 @@ use rshooks_api::*;
 const GUARD_ID_1: u32 = line!();
 
 #[no_mangle]
-pub extern "C" fn cbak(_: u32) -> i64 { 
+pub extern "C" fn cbak(_: u32) -> i64 {
     return 0;
 }
 
 #[no_mangle]
 pub extern "C" fn hook(_: i64) -> i64 {
-    // Every hook needs to import guard function 
+    // Every hook needs to import guard function
     // and use it at least once
     _g(GUARD_ID_1, 1);
-    
+
     let mut account: AccountId = uninit_buf!();
     let _ = hook_account(&mut account);
-    
+
     let mut raddr: Buffer<40> = uninit_buf!();
     let _ = util_raddr(&mut raddr, &account);
 
     // Tracing when compiling in debug mode
     #[cfg(debug_assertions)]
-    let _  = trace(b"Accept: called", &raddr, DataRepr::AsUTF8);
+    let _ = trace(b"Accept: called", &raddr, DataRepr::AsUTF8);
 
     // Accept all
     accept(b"Accept: accepted", 0)
