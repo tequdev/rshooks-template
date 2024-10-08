8137765daa10a6e924f8dbd300c3dece70736cc1
tequ <git@tequ.dev>
2024-10-08 16:33:43 +0900

    fix to use `allow(unused_must_use)`

--- src/lib.rs
+++ src/lib.rs
@@ -12,20 +12,21 @@ pub extern "C" fn cbak(_: u32) -> i64 {
 }
 
 #[no_mangle]
+#[allow(unused_must_use)]
 pub extern "C" fn hook(_: i64) -> i64 {
     // Every hook needs to import guard function
     // and use it at least once
     _g(GUARD_ID_1, 1);
 
     let mut account: AccountId = uninit_buf!();
-    let _ = hook_account(&mut account);
+    hook_account(&mut account);
 
     let mut raddr: Buffer<40> = uninit_buf!();
-    let _ = util_raddr(&mut raddr, &account);
+    util_raddr(&mut raddr, &account);
 
     // Tracing when compiling in debug mode
     #[cfg(debug_assertions)]
-    let _ = trace(b"Accept: called", &raddr, DataRepr::AsUTF8);
+    trace(b"Accept: called", &raddr, DataRepr::AsUTF8);
 
     // Accept all
     accept(b"Accept: accepted", 0)
